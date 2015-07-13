bd87e5c93b85c117d38026fc2b06f28b2324c509
Antoine <antoine.garcia@epitech.eu>
2015-07-13 13:54:06 +0200

    Add Strasbourg + Update NCE

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var app = angular.module('myApp', []);
 
 app.controller('RankCtrl', function($scope, $http) {
 	$scope.users = [];
-	$scope.cities = {"Nice" : "NCE", "Montpellier": "MPL", "Marseille": "MAR"}
+	$scope.cities = {"Nice" : "NCE", "Montpellier": "MPL", "Marseille": "MAR", "Strasbourg" : "STG"}
 	$scope.citySelect = "Nice"
 	openCityJson($scope.citySelect, $http, $scope)
     $scope.$watch('citySelect',function(){
