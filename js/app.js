01c3b3cfe7ee0a1bafeca977c30e758a39b08cc4
Antoine <antoine.garcia@epitech.eu>
2015-07-13 11:17:02 +0200

    add Marseille

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var app = angular.module('myApp', []);
 
 app.controller('RankCtrl', function($scope, $http) {
 	$scope.users = [];
-	$scope.cities = {"Nice" : "NCE", "Montpellier": "MPL"}
+	$scope.cities = {"Nice" : "NCE", "Montpellier": "MPL", "Marseille": "MAR"}
 	$scope.citySelect = "Nice"
 	openCityJson($scope.citySelect, $http, $scope)
     $scope.$watch('citySelect',function(){
